053ec5eb81358477b31604accf795cafdd19744b
JuanTadini <juan.tadini@gmail.com>
2023-11-02 19:51:15 -0300

    cambiadas algunas rutas de imagenes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
   return (
     <footer className={styles["footer"]}>
 		<div className={styles["logoFooter"]}>
-		  	<Link to="/"> <img className={styles["logo1"]} src="./../public/imagenes/logo1.png" alt="logo1" alt="logo" /></Link>
+		  	<Link to="/"> <img className={styles["logo1"]} src="/imagenes/logo1.png" alt="logo1" /></Link>
 			<p>Copyright © 2023 CumbreSki. Todos los derechos reservados.</p>
 		</div>
 		<div className={styles["footer-redes"]}>
-			<img className={styles["footer-redes-icono"]} src="../../public/imagenes/ico-facebook.png" alt="facebook" />
-			<img className={styles["footer-redes-icono"]} src="../../public/imagenes/ico-instagram.png" alt="instagram" />
-			<img className={styles["footer-redes-icono"]} src="../../public/imagenes/ico-tiktok.png" alt="tiktok" />
-			<img className={styles["footer-redes-icono"]} src="../../public/imagenes/ico-whatsapp.png" alt="whatsapp" />
+			<img className={styles["footer-redes-icono"]} src="/imagenes/ico-facebook.png" alt="facebook" />
+			<img className={styles["footer-redes-icono"]} src="/imagenes/ico-instagram.png" alt="instagram" />
+			<img className={styles["footer-redes-icono"]} src="/imagenes/ico-tiktok.png" alt="tiktok" />
+			<img className={styles["footer-redes-icono"]} src="/imagenes/ico-whatsapp.png" alt="whatsapp" />
 		</div>
 	</footer>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
   return (
     <nav className={styles['nav']}>
       <div className={styles['logo']}>
-        <Link to="/"> <img className={styles["logo1"]} src="./../public/imagenes/logo1.png" alt="logo1" alt="logo" /></Link>
+        <Link to="/"> <img className={styles["logo1"]} src="/imagenes/logo1.png" alt="logo1" /></Link>
         <p>Vivi la temporada de invierno en la nieve</p>
       </div>
         <Link to="/buscar">BUSCADOR</Link>
         <Link to="/recomendation">RECOMENDACIONES</Link>
       <div className={styles['crearIniciar']}>
-        <button className={styles['crear']}><img className={styles["vector1"]} src="../../public/imagenes/vector1.png" alt="vector1" />  Crear Cuenta</button>
-        <button className={styles['crear']}><img className={styles["vector1"]} src="../../public/imagenes/navigation.png" alt="vector1" /> Iniciar Seccion</button>
+        <button className={styles['crear']}><img className={styles["vector1"]} src="/imagenes/vector1.png" alt="vector1" />  Crear Cuenta</button>
+        <button className={styles['crear']}><img className={styles["vector1"]} src="/imagenes/navigation.png" alt="vector1" /> Iniciar Seccion</button>
       </div>
     </nav>
   )
